22454f4d89eaddce615f4656f182b03b8335ecaf
Emilian Frauhammer <emilian.frauhammer@protonmail.com>
2019-11-26 20:39:30 +0100

    it's no longer completely dumb

--- i3cmds/lock.sh
+++ i3cmds/lock.sh
@@ -2,9 +2,10 @@
 
 sflock -c "epsteindidntkillhimself"
 i=30
-while [ $i > 0 ]
+while [ $i != 0 ]
 do
-    pgrep sflock > /dev/null || break
+    pgrep sflock > /dev/null || exit 1
     sleep 60
+    i=$i-1
 done
 systemctl suspend
